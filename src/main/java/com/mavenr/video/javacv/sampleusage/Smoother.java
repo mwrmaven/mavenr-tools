b9289e65a013e110315635da18552b0f696107b7
马文瑞 <1227156208@qq.com>
2021-06-23 18:01:20 +0800

    simple use of javacv

---
+++ src/main/java/com/mavenr/video/javacv/sampleusage/Smoother.java
@@ -0,0 +1,11 @@
+package com.mavenr.video.javacv.sampleusage;
+
+/**
+ * @author mavenr
+ * @Classname Smoother
+ * @Description simple use of javacv
+ * @Date 2021/6/23 17:59
+ */
+public class Smoother {
+
+}
