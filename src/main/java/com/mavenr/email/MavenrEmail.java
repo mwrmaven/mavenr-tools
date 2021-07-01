31cf080dbba4702d88396738b37e05bc815a37ed
马文瑞 <mavenr@mawenruideMacBook-Pro.local>
2021-07-01 23:24:34 +0800

    添加邮件接口，并简单实现QQ邮件服务

---
+++ src/main/java/com/mavenr/email/MavenrEmail.java
@@ -0,0 +1,21 @@
+package com.mavenr.email;
+
+import java.util.List;
+
+/**
+ * @Author mavenr
+ * @Classname  MavenrEmail
+ * @Description 自定义邮件接口
+ * @Date 2021/7/1 6:26 下午
+ */
+public interface MavenrEmail {
+
+    /**
+     * 发送普通邮件
+     * @param from 发送人邮箱
+     * @param to 接收人邮箱
+     * @param subject 邮件主题
+     * @param content 邮件内容
+     */
+    void sendMail(String from, List<String> to, String subject, String content) throws Exception;
+}
---
+++ src/main/java/com/mavenr/email/MavenrQQEmail.java
@@ -0,0 +1,85 @@
+package com.mavenr.email;
+
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * @Author mavenr
+ * @Classname  MavenrQQEmail
+ * @Description QQ邮件实现类
+ * @Date 2021/7/1 7:00 下午
+ */
+public class MavenrQQEmail implements MavenrEmail{
+
+    private String username;
+
+    private String password;
+
+    public MavenrQQEmail(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * 发送普通邮件(多接收人)
+     * @param from 发送人邮箱
+     * @param to 接收人邮箱
+     * @param subject 邮件主题
+     * @param content 邮件内容
+     */
+    @Override
+    public void sendMail(String from, List<String> to, String subject, String content) {
+        // 设置QQ邮箱参数
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.host", "smtp.qq.com");
+        properties.put("mail.smtp.port", 465);
+        properties.put("mail.smtp.ssl.enable", true);
+
+        // 创建Session会话
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        // 逗号拼接地址
+        StringBuilder addresses = new StringBuilder();
+        for (String t : to) {
+            addresses.append(t).append(",");
+        }
+        String realAddresses = addresses.substring(0, addresses.length() - 1);
+        // 创建消息
+        Message message = new MimeMessage(session);
+        try {
+            message.setFrom(new InternetAddress(from));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(realAddresses));
+            message.setSubject(subject);
+            message.setText(content);
+            // 发送邮件
+            Transport.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 发送普通邮件(单个接收人)
+     * @param from 发送人邮箱
+     * @param to 接收人邮箱
+     * @param subject 邮件主题
+     * @param content 邮件内容
+     */
+    public void sendMail(String from, String to, String subject, String content) {
+        List<String> tos = new ArrayList<>();
+        tos.add(to);
+        sendMail(from, tos, subject, content);
+    }
+}
