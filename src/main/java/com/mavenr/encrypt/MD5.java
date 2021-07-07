c8588b8a55267271b42ec9839a7a4560a417f96a
1227156208@qq.com <1227156208@qq.com>
2021-07-07 16:14:32 +0800

    添加sha和md5的加密算法；添加微信验证接口

---
+++ src/main/java/com/mavenr/encrypt/MD5.java
@@ -0,0 +1,37 @@
+package com.mavenr.encrypt;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author mavenr
+ * @Classname MD5
+ * @Description MD5加解密算法
+ * @Date 2021/7/7 16:04
+ */
+public class MD5 {
+
+    /**
+     * 加密
+     * @param key
+     * @return
+     */
+    public static String encode(String key) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            byte[] digest = messageDigest.digest(key.getBytes());
+            StringBuilder tmp = new StringBuilder();
+            for (int i = 0; i < digest.length; i++) {
+                int bt = digest[i] & 0xFF;
+                if (bt < 16) {
+                    tmp.append("0");
+                }
+                tmp.append(Integer.toHexString(bt));
+            }
+            return tmp.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
