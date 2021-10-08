874bf2c0a99d02410a4c715c80082895e5d7eba4
1227156208@qq.com <1227156208@qq.com>
2021-10-08 09:19:15 +0800

    add class FileContentSort

---
+++ src/main/java/com/mavenr/file/FileContentSort.java
@@ -0,0 +1,118 @@
+package com.mavenr.file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author mavenr
+ * @Classname FileContentSort
+ * @Description 文件内容排序，并输出到新文件
+ *              sort the contents of the file and output to the new file
+ * @Date 2021/10/8 9:07
+ */
+public class FileContentSort {
+    /**
+     * 以文本行中的分隔符排序
+     * sort by separator in text line of the file
+     * @param files the source files
+     * @param splitStr separator in text line
+     * @param num index of text which is separated
+     */
+    public static void sortText(List<File> files, String splitStr, int num) {
+        for (File f : files) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(f));
+                String line;
+                Map<String, String> map = new HashMap<>();
+                while ((line = br.readLine()) != null) {
+                    map.put(line.split(splitStr)[num - 1], line);
+                }
+
+                List<String> keys = map.keySet().stream().sorted(new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        return o1.compareTo(o2);
+                    }
+                }).collect(Collectors.toList());
+                StringBuilder sb = new StringBuilder();
+                for (String k : keys) {
+                    sb.append(map.get(k)).append("\n");
+                }
+                String path = f.getPath();
+                File newFile = new File(path.substring(0, path.lastIndexOf(".")) + "_new" + path.substring(path.lastIndexOf(".")));
+                newFile.createNewFile();
+                FileChannel fc = new FileOutputStream(newFile).getChannel();
+                ByteBuffer buffer = ByteBuffer.allocate(40960);
+                buffer.put(sb.toString().getBytes());
+                buffer.flip();
+                fc.write(buffer);
+                fc.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 以文本行中字符的范围区间排序
+     * sort by the range of characters in the text line
+     * @param files the source files
+     * @param start start index
+     * @param end end index
+     */
+    public static void sortText(List<File> files, int start, int end) {
+        for (File f : files) {
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(f));
+                String line;
+                Map<String, String> map = new HashMap<>();
+                while ((line = br.readLine()) != null) {
+                    if (start < 0) {
+                        start = 0;
+                    }
+                    if (end < start) {
+                        end = start;
+                    }
+                    if (end >= line.length()) {
+                        map.put(line.substring(start - 1), line);
+                    } else {
+                        map.put(line.substring(start - 1, end), line);
+                    }
+                }
+
+                List<String> keys = map.keySet().stream().sorted(new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        return o1.compareTo(o2);
+                    }
+                }).collect(Collectors.toList());
+                StringBuilder sb = new StringBuilder();
+                for (String k : keys) {
+                    sb.append(map.get(k)).append("\n");
+                }
+                String path = f.getPath();
+                File newFile = new File(path.substring(0, path.lastIndexOf(".")) + "_new" + path.substring(path.lastIndexOf(".")));
+                newFile.createNewFile();
+                FileChannel fc = new FileOutputStream(newFile).getChannel();
+                ByteBuffer buffer = ByteBuffer.allocate(40960);
+                buffer.put(sb.toString().getBytes());
+                buffer.flip();
+                fc.write(buffer);
+                fc.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
